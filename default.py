3df480e51e4f0a436fa78a420a060b345fd87f16
Martin Polednik <mpolednik@redhat.com>
2016-05-23 18:54:56 +0200

    Refactor HSV spectrum calculations
    The code responsible for creating HSV spectrum did unnecessary looping
    and was needlessly complex. We minimize the loops (still based on
    previous logic, not changing anything significant) to improve the
    performance and code.

--- default.py
+++ default.py
@@ -477,28 +477,24 @@ class Screenshot:
   def most_used_spectrum(self, spectrum, saturation, value, size, overall_value):
     # color bias/groups 6 - 36 in steps of 3
     colorGroups = settings.color_bias
-    if colorGroups == 0:
-      colorGroups = 1
     colorHueRatio = 360 / colorGroups
 
     hsvRatios = []
     hsvRatiosDict = {}
 
-    for i in range(360):
-      if spectrum.has_key(i):
-        #shift index to the right so that groups are centered on primary and secondary colors
-        colorIndex = int(((i+colorHueRatio/2) % 360)/colorHueRatio)
-        pixelCount = spectrum[i]
+    for i in spectrum:
+      # shift index to the right so that groups are centered on primary and secondary colors
+      colorIndex = int(((i+colorHueRatio/2) % 360)/colorHueRatio)
+      pixelCount = spectrum[i]
 
-        if hsvRatiosDict.has_key(colorIndex):
-          hsvr = hsvRatiosDict[colorIndex]
-          hsvr.average(i/360.0, saturation[i], value[i])
-          hsvr.ratio = hsvr.ratio + pixelCount / float(size)
-
-        else:
-          hsvr = HSVRatio(i/360.0, saturation[i], value[i], pixelCount / float(size))
-          hsvRatiosDict[colorIndex] = hsvr
-          hsvRatios.append(hsvr)
+      try:
+        hsvr = hsvRatiosDict[colorIndex]
+        hsvr.average(i/360.0, saturation[i], value[i])
+        hsvr.ratio = hsvr.ratio + pixelCount / float(size)
+      except KeyError:
+        hsvr = HSVRatio(i/360.0, saturation[i], value[i], pixelCount / float(size))
+        hsvRatiosDict[colorIndex] = hsvr
+        hsvRatios.append(hsvr)
 
     colorCount = len(hsvRatios)
     if colorCount > 1:
@@ -519,8 +515,7 @@ class Screenshot:
       hsvRatios[0].averageValue(overall_value)
       return [hsvRatios[0]] * 3
 
-    else:
-      return [HSVRatio()] * 3
+    return [HSVRatio()] * 3
 
   def spectrum_hsv(self, pixels, width, height):
     spectrum = {}
@@ -528,48 +523,43 @@ class Screenshot:
     value = {}
 
     size = int(len(pixels)/4)
-    pixel = 0
 
-    i = 0
-    s, v = 0, 0
+    v = 0
     r, g, b = 0, 0, 0
     tmph, tmps, tmpv = 0, 0, 0
 
-    for i in range(size):
-      if fmtRGBA:
-        r = pixels[pixel]
-        g = pixels[pixel + 1]
-        b = pixels[pixel + 2]
-      else: #probably BGRA
-        b = pixels[pixel]
-        g = pixels[pixel + 1]
-        r = pixels[pixel + 2]
-      pixel += 4
-
+    for i in range(0, size, 4):
+      r, g, b = _rgb_from_pixels(pixels, i)
       tmph, tmps, tmpv = colorsys.rgb_to_hsv(float(r/255.0), float(g/255.0), float(b/255.0))
-      s += tmps
       v += tmpv
 
       # skip low value and saturation
       if tmpv > 0.25:
         if tmps > 0.33:
           h = int(tmph * 360)
+          try:
+              spectrum[h] += 1
+              saturation[h] = (saturation[h] + tmps)/2
+              value[h] = (value[h] + tmpv)/2
+          except KeyError:
+              spectrum[h] = 1
+              saturation[h] = tmps
+              value[h] = tmpv
 
-          # logger.debuglog("%s \t set pixel r %s \tg %s \tb %s" % (i, r, g, b))
-          # logger.debuglog("%s \t set pixel h %s \ts %s \tv %s" % (i, tmph*100, tmps*100, tmpv*100))
-
-          if spectrum.has_key(h):
-            spectrum[h] += 1 # tmps * 2 * tmpv
-            saturation[h] = (saturation[h] + tmps)/2
-            value[h] = (value[h] + tmpv)/2
-          else:
-            spectrum[h] = 1 # tmps * 2 * tmpv
-            saturation[h] = tmps
-            value[h] = tmpv
-
-    overall_value = v / float(i)
-    # s_overall = int(s * 100 / i)
+    overall_value = v / float(len(pixels))
     return self.most_used_spectrum(spectrum, saturation, value, size, overall_value)
+
+
+def _rgb_from_pixels(pixels, index):
+  if fmtRGBA:
+    return _rgb_from_pixels_rgba(pixels, index)
+  else:  # probably BGRA
+    return _rgb_from_pixels_rgba(pixels, index)[::-1]
+
+
+def _rgb_from_pixels_rgba(pixels, index):
+  return [pixels[index + i] for i in range(3)]
+
 
 def run():
   player = None
