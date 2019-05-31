4ed1caf00c808c64e508cdc05925e0fb853bd211
zim514 <krisyoung@gmail.com>
2019-05-31 15:43:03 -0400

    add logging globals

--- script.service.hue/resources/lib/globals.py
+++ script.service.hue/resources/lib/globals.py
@@ -3,19 +3,27 @@ Created on Apr. 22, 2019
 
 
 '''
+global settingsChanged
+global connected 
+global forceOnSunset
+global daylightDisable
+global separateLogFile
+global logLevel
+
 
 settingsChanged = False
 connected = False
 daylight = False
 forceOnSunset = True
 daylightDisable = True
+separateLogFile = False
+logLevel = 0
+
 
 NUM_GROUPS = 1
-
-STRDEBUG = False
-
-DEBUG = False
-REMOTE_DBG_SUSPEND = False
+STRDEBUG = False #Show string ID in UI 
+DEBUG = False # Enable python remote debug
+REMOTE_DBG_SUSPEND = False #Auto suspend thread when debugger attached
 
 
 
