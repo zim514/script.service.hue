8946283012fb99bcd0b0d73b07ae3aeac516ca9b
zim514 <krisyoung@gmail.com>
2019-07-14 18:35:47 -0400

    ambilight part 2 - support hue gamuts - rgb blacks & whites cut off support

--- script.service.hue/resources/lib/AmbiGroup.py
+++ script.service.hue/resources/lib/AmbiGroup.py
@@ -11,7 +11,7 @@ from threading import Thread
 
 
 from PIL import Image
-import colorgram #https://github.com/obskyr/colorgram.py
+from . import colorgram #https://github.com/obskyr/colorgram.py
 from .rgbxy import Converter# https://github.com/benknight/hue-python-rgb-converter
 from .rgbxy import ColorHelper
 from .rgbxy import XYPoint
@@ -20,16 +20,17 @@ from .rgbxy import GamutA,GamutB,GamutC
 import xbmc
 
 from resources.lib.KodiGroup import KodiGroup
-from resources.lib.KodiGroup import VIDEO,ALLMEDIA,AUDIO,STATE_IDLE,STATE_PAUSED,STATE_PLAYING
+from resources.lib.KodiGroup import VIDEO,AUDIO,ALLMEDIA,STATE_IDLE,STATE_PAUSED,STATE_PLAYING
 
 
 from . import kodiutils
 #from .kodiutils import get_setting,get_setting_as_bool,get_setting_as_int,get_setting_as_float,convertTime
 from .qhue import QhueException
 
-import globals
-from globals import logger
+from . import globals
+from .globals import logger
 from .language import get_string as _
+
 
 
 
@@ -81,9 +82,23 @@ class AmbiGroup(KodiGroup):
         self.forceOn=kodiutils.get_setting_as_bool("group{}_forceOn".format(self.kgroupID))
         self.setBrightness=kodiutils.get_setting_as_bool("group{}_setBrightness".format(self.kgroupID))
         self.brightness=kodiutils.get_setting_as_int("group{}_Brightness".format(self.kgroupID))*255/100#convert percentage to value 1-254
+        self.blackFilter=kodiutils.get_setting_as_int("group{}_BlackFilter".format(self.kgroupID))
+        self.whiteFilter=kodiutils.get_setting_as_int("group{}_WhiteFilter".format(self.kgroupID))
+        
+        
         
         #self.lights=kodiutils.get_setting("group{}_Interval".format(self.kgroupID))
-        self.ambiLights=list(map(int,kodiutils.get_setting("group{}_Lights".format(self.kgroupID)).split(",")))
+        self.ambiLights={}
+        lightIDs=kodiutils.get_setting("group{}_Lights".format(self.kgroupID)).split(",")
+        for L in lightIDs:
+
+            gamut=self._getLightGamut(self.bridge,L)
+            light={L:{'gamut': gamut,'prevxy': (0,0)}}
+            self.ambiLights.update(light)
+            
+
+        #read gamuts, make this a dict
+        logger.debug("ambilights obj: {}".format(self.ambiLights))
     
     
     def setup(self, monitor,bridge, kgroupID, flash=False, mediaType=VIDEO):
@@ -101,51 +116,48 @@ class AmbiGroup(KodiGroup):
         pass
 
     def _ambiLoop(self):
-        converter=Converter(GamutC)
-        helper=ColorHelper(GamutC)
+        
         cap = xbmc.RenderCapture()
         
         
         distance=0.0
-        xy=0.51,0.41
-        prevxy=0.51,0.41
+        self.xy=0.5266,0.4133
+        self.prevxy=0.5266,0.4133
         logger.debug("AmbiGroup started!")
+        
         while not self.monitor.abortRequested() and self.state == STATE_PLAYING:
             startTime = time.time()
-            cap.capture(250, 250) #async capture request to underlying OS
-            capImage = cap.getImage() #timeout to wait for OS in ms, default 1000 
-            
-            image = Image.frombuffer("RGBA", (250, 250), buffer(capImage), "raw", "BGRA")
+             
+            try:
+                cap.capture(250, 250) #async capture request to underlying OS
+                capImage = cap.getImage() #timeout to wait for OS in ms, default 1000
+                image = Image.frombuffer("RGBA", (250, 250), buffer(capImage), "raw", "BGRA")
+            except Exception:
+                return #avoid fails in system shutdown
+                
             
             
             colors = colorgram.extract(image,self.numColors)
             #TODO: RGB min and max configurable.
-            if colors[0].rgb.r < 10 and colors[0].rgb.g < 10 and colors[0].rgb.b <10:
-                xy=0.5266,0.4133 #max warmth for Gamut C, converted from CT to XY
-                #xy=converter.rgb_to_xy(1,1,1)
-            else:
-                xy=converter.rgb_to_xy(colors[0].rgb.r,colors[0].rgb.g,colors[0].rgb.b)
-                xy=(round(xy[0],4),round(xy[1],4)) #Hue has a max precision of 4 decimal points.
-            #self._updateHue(xy,2)
-            distance=helper.get_distance_between_two_points(XYPoint(xy[0],xy[1]),XYPoint(prevxy[0],prevxy[1])) #only update hue if XY actually changed
-            if distance > 0: 
+            if (colors[0].rgb.r < self.blackFilter and colors[0].rgb.g < self.blackFilter and colors[0].rgb.b <self.blackFilter) or \
+            (colors[0].rgb.r > self.whiteFilter and colors[0].rgb.g > self.whiteFilter and colors[0].rgb.b > self.whiteFilter):
+                logger.debug("rgb filter: r,g,b: {},{},{}".format(colors[0].rgb.r,colors[0].rgb.g,colors[0].rgb.b))
+                
+                xy=0.5266,0.4133 #default
+                
                 for L in self.ambiLights: 
-                    x = Thread(target=self._updateHue,name="updateHue", args=(xy,L,self.transitionTime))
+                    x = Thread(target=self._updateHueXY,name="updateHue", args=(xy,L,self.transitionTime))
+                    x.daemon = True
+                    x.start()
+                
+            else:
+                for L in self.ambiLights: 
+                    x = Thread(target=self._updateHueRGB,name="updateHue", args=(colors[0].rgb.r,colors[0].rgb.g,colors[0].rgb.b,L,self.transitionTime))
                     x.daemon = True
                     x.start()
             
             endTime= time.time()
-            
-            
-            if distance > 0:
-                #logger.debug("time: {},Colors: {}, xy: {},prevxy:{}, distance: {}".format(endTime-startTime,colors,xy,prevxy,distance))
-                #logger.debug("***** xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
-                pass
-            else:
-                #logger.debug("xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
-                pass
 
-            prevxy=xy
             self.monitor.waitForAbort(self.updateInterval) #seconds
 
         logger.debug("AmbiGroup stopped!")
@@ -153,14 +165,110 @@ class AmbiGroup(KodiGroup):
         
         
     
-    def _updateHue(self,xy,light,transitionTime):
-        #startTime = time.time()
-        try:
-            self.bridge.lights[light].state(xy=xy,transitiontime=transitionTime)
-        except QhueException as e:
-            logger.error("Ambi: Hue call fail: {}".format(e))
+    def _updateHueRGB(self,r,g,b,light,transitionTime):
+        startTime = time.time()
         
-        #endTime=time.time()
-        #logger.debug("_updateHue time: {}".format(endTime-startTime))
+        gamut=self.ambiLights[light].get('gamut')
+        prevxy=self.ambiLights[light].get('prevxy')
+        
+        if gamut == "A":
+            converter=Converter(GamutA)
+            helper=ColorHelper(GamutA)
+        elif gamut == "B":
+            converter=Converter(GamutB)
+            helper=ColorHelper(GamutB)
+        elif gamut == "C":
+            converter=Converter(GamutC)
+            helper=ColorHelper(GamutC)
+        
+        xy=converter.rgb_to_xy(r,g,b)
+        xy=(round(xy[0],4),round(xy[1],4)) #Hue has a max precision of 4 decimal points.
+
+        distance=round(helper.get_distance_between_two_points(XYPoint(xy[0],xy[1]),XYPoint(prevxy[0],prevxy[1])) ,4)#only update hue if XY actually changed
+        if distance > 0:
+            try:
+                self.bridge.lights[light].state(xy=xy,transitiontime=transitionTime)
+            except QhueException as e:
+                logger.error("Ambi: Hue call fail: {}".format(e))
+        
+        #=======================================================================
+        endTime=time.time()
+        # 
+        # 
+        
+        #logger.debug("_updateHue time: {}".format(int((endTime-startTime)*1000)))
+        
+        logger.debug("time: {},distance: {}".format(int((endTime-startTime)*1000),distance))
+        # if distance > 0:
+        #     #logger.debug("time: {},Colors: {}, xy: {},prevxy:{}, distance: {}".format(endTime-startTime,colors,xy,prevxy,distance))
+        #     #logger.debug("***** xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
+        #     pass
+        # else:
+        #     #logger.debug("* xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
+        #     pass
+        #  
+        # 
+        # 
+        #=======================================================================
+        self.ambiLights[light].update(prevxy=xy)
+        
+
+    def _updateHueXY(self,xy,light,transitionTime):
+        startTime = time.time()
+        
+        gamut=self.ambiLights[light].get('gamut')
+        prevxy=self.ambiLights[light].get('prevxy')
+        
+        if gamut == "A":
+            helper=ColorHelper(GamutA)
+        elif gamut == "B":
+            helper=ColorHelper(GamutB)
+        elif gamut == "C":
+            helper=ColorHelper(GamutC)
+        
+
+        xy=(round(xy[0],4),round(xy[1],4)) #Hue has a max precision of 4 decimal points.
+
+        distance=round(helper.get_distance_between_two_points(XYPoint(xy[0],xy[1]),XYPoint(prevxy[0],prevxy[1])) ,4)#only update hue if XY actually changed
+        if distance > 0:
+            try:
+                self.bridge.lights[light].state(xy=xy,transitiontime=transitionTime)
+            except QhueException as e:
+                logger.error("Ambi: Hue call fail: {}".format(e))
+        
+        #=======================================================================
+        endTime=time.time()
+        # 
+        # 
+        
+        #logger.debug("_updateHue time: {}".format(int((endTime-startTime)*1000)))
+        
+        logger.debug("time: {},distance: {}".format(int((endTime-startTime)*1000),distance))
+        # if distance > 0:
+        #     #logger.debug("time: {},Colors: {}, xy: {},prevxy:{}, distance: {}".format(endTime-startTime,colors,xy,prevxy,distance))
+        #     #logger.debug("***** xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
+        #     pass
+        # else:
+        #     #logger.debug("* xy: {},prevxy:{}, distance: {}".format(xy,prevxy,distance))
+        #     pass
+        #  
+        # 
+        # 
+        #=======================================================================
+        self.ambiLights[light].update(prevxy=xy)
+
+
+
+
+    def _getLightGamut(self,bridge,L):
+        try:
+            gamut = bridge.lights()[L]['capabilities']['control']['colorgamuttype']
+            logger.debug("Light: {}, gamut: {}".format(L,gamut))
+        except Exception:
+            return None
+        if gamut == "A"  or gamut == "B" or gamut == "C":
+            return gamut
+        return None
+        
 
 
