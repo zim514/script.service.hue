d33b094f1addfe8e639b9fffaf89f5eca3c505d0
Snapcase <krisyoung@gmail.com>
2019-09-25 15:47:24 -0400

    remove colorgram

--- script.service.hue/resources/lib/colorgram/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# colorgram.py, a module to extract colors from images.
-# Based on Jan Forst's original JavaScript version.
-
-from __future__ import absolute_import
-
-from .colorgram import extract, Color
