b7115aed40fbb72aa107ac08c903430fd4a03dfa
zim514 <krisyoung@gmail.com>
2022-06-24 10:06:45 -0400

    fix a broken log line

--- script.service.hue/resources/lib/ambigroup.py
+++ script.service.hue/resources/lib/ambigroup.py
@@ -167,7 +167,7 @@ class AmbiGroup(lightgroup.LightGroup):
                 cap_image = cap.getImage()  # timeout to wait for OS in ms, default 1000
 
                 if cap_image is None or len(cap_image) < expected_capture_size:
-                    xbmc.log("[script.service.hue] capImage is none or < expected. captured: {}, expected: {}".format(len(capImage), expected_capture_size))
+                    xbmc.log("[script.service.hue] capImage is none or < expected. captured: {}, expected: {}".format(len(cap_image), expected_capture_size))
                     xbmc.sleep(250)  # pause before trying again
                     continue  # no image captured, try again next iteration
                 image = Image.frombytes("RGBA", (self.capture_size_x, self.capture_size_y), bytes(cap_image), "raw", "BGRA", 0, 1)  # Kodi always returns a BGRA image.
